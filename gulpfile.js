88ab00a318950e3c14cdb48b9638c470bd14c126
Angel Ivanov <a_ivanovvv@abv.bg>
2017-09-24 20:03:15 +0300

    edit gulp task foleder

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,3 @@
-require('./gulp/tasks/html');
-require('./gulp/tasks/styles');
-require('./gulp/tasks/watch');
+require('./gulp_tasks/html');
+require('./gulp_tasks/styles');
+require('./gulp_tasks/watch');
